e17dd0591b3703e7ab5403c3eadbbc2845b0c2d0
Nguyentnl <nguyentnl295@gmail.com>
2018-07-29 14:24:43 +0300

    added projects

---
+++ count_sequence.py
@@ -0,0 +1,13 @@
+a = raw_input('Enter a sentence or a list: ')
+b = raw_input ('Enter a thing to check: ')
+
+
+def count(sequence,item):
+    count = 0
+    for i in sequence:  # type: int
+        if i == item:
+            count += 1
+    return count
+
+
+print count(a, b)
